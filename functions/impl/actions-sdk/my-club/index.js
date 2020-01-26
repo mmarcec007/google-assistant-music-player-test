311cdeb4e2781b52f414fdd4883761a9cdf69847
Mark Marcec <mark.marcec@gmail.com>
2020-01-26 16:18:33 +0100

    add more clever trigger intent handling for new conversations

--- functions/impl/actions-sdk/my-club/index.js
+++ functions/impl/actions-sdk/my-club/index.js
@@ -11,8 +11,27 @@ exports.myClubImpl = (req, resp) => {
 
         // handles the trigger query
         if (userInputs[0].arguments && userInputs[0].arguments[0].name === 'trigger_query') {
-            response = actionsSdkResponse.getTableResponse("Here are the results of the following match:");
-        } else {
+            const intent = userInputs[0].intent;
+            console.log("Resolved intent: " + intent);
+
+            if (intent === 'actions.intent.TEXT') {
+                text = "Please include more than one intents in you actions package!";
+                response = actionsSdkResponse.getSimpleResponse(text, false)
+            } else {
+                if (intent === 'com.mark.dev.MATCH_RESULTS') {
+                    text = "Here are the results of the following match:";
+                    response = actionsSdkResponse.getTableResponse(text);
+                } else if (intent === 'com.mark.dev.MATCHES') {
+                    text = "Here are the requested matches:";
+                    response = actionsSdkResponse.getTableResponse(text);
+                } else {
+                    text = "Unknown trigger query. Please try again later!";
+                    response = actionsSdkResponse.getSimpleResponse(text, false)
+                }
+            }
+        }
+        // otherwise add a greeting message
+        else {
             response = actionsSdkResponse.getSimpleResponse(text, true);
         }
     } else if (conversation.type === 'ACTIVE') {
