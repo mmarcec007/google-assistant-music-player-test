a7cd8b6427cb2b1f8b87318471d2f87c90594572
Mark Marcec <mark@zef.dev>
2020-01-31 15:42:28 +0100

    add audio handling in dialogflow webhook implementation

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,16 @@ exports.webhook = functions.https.onRequest((req, resp) => {
     myClub.myClubImpl(req, resp);
 });
 
+const mp3Files = [
+    "https://mp3-utopian-plane.s3.eu-central-1.amazonaws.com/01-freeky.mp3",
+    "https://mp3-utopian-plane.s3.eu-central-1.amazonaws.com/02-hot.mp3",
+    "https://mp3-utopian-plane.s3.eu-central-1.amazonaws.com/02-kitty-doesnt-like-320kbps.mp3",
+    "https://mp3-utopian-plane.s3.eu-central-1.amazonaws.com/03-shit.mp3",
+    "https://mp3-utopian-plane.s3.eu-central-1.amazonaws.com/03-talking-kitty-cat-stupid-stupid-world-320kbps.mp3",
+    "https://mp3-utopian-plane.s3.eu-central-1.amazonaws.com/04-nowhere-az-few-far-between-320kbps.mp3",
+    "https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3"
+];
+let currentIndex = 0;
 exports.dialogFlowWebhook = functions.https.onRequest((req, resp) => {
     let result = dialogFlowResponse.getSimpleResponse("I don't understand you well.", true);
     const conversation = req.body.originalDetectIntentRequest.payload.conversation;
@@ -20,8 +30,30 @@ exports.dialogFlowWebhook = functions.https.onRequest((req, resp) => {
     if (conversation.type === 'NEW') {
         result = dialogFlowResponse.getSimpleResponse("Hi there! Welcome to Custom Music Player on Dialogflow.", true);
     } else if (conversation.type === 'ACTIVE') {
-        result = dialogFlowResponse.getSimpleResponse("Lets play some music!", true);
-    }
+        const userInput = req.body.queryResult.queryText;
+        const detectedIntent = req.body.queryResult.intent;
 
+
+        if (detectedIntent.displayName === "Media") {
+            result = dialogFlowResponse.getMediaResponse("Here you go:", mp3Files[currentIndex]);
+        } else if (detectedIntent.displayName === "Next") {
+            console.log("Next");
+            currentIndex++;
+            if (currentIndex >= mp3Files.length) {
+                currentIndex = 0;
+            }
+            result = dialogFlowResponse.getMediaResponse("Playing next title:", mp3Files[currentIndex]);
+        } else if (detectedIntent.displayName === "Previous") {
+            currentIndex++;
+            if (currentIndex <= mp3Files.length) {
+                currentIndex = 0;
+            }
+            result = dialogFlowResponse.getMediaResponse("Playing previous title:", mp3Files[currentIndex]);
+            console.log("Previous");
+        } else {
+            result = dialogFlowResponse.getSimpleResponse(userInput, true);
+        }
+    }
+    dialogFlowResponse.log(req, result);
     resp.send(result);
 });
