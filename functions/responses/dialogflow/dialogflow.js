50d18c0736a819336b46ea27f8d72a7fac43c43d
Mark Marcec <mark@zef.dev>
2020-01-31 15:41:58 +0100

    add media response and log to dialogflow.js response

--- functions/responses/dialogflow/dialogflow.js
+++ functions/responses/dialogflow/dialogflow.js
@@ -18,3 +18,49 @@ exports.getSimpleResponse = (simpleResponseText, expectUserInput) => {
         }
     };
 };
+
+exports.getMediaResponse = (text, mp3Url) => {
+    const fileName = mp3Url.substring(mp3Url.lastIndexOf('/')+1);
+    return {
+      "payload": {
+          "google": {
+              "expectUserResponse": true,
+              "richResponse": {
+                  "items": [
+                      {
+                          "simpleResponse": {
+                              "textToSpeech": text
+                          }
+                      },
+                      {
+                          "mediaResponse": {
+                              "mediaType": "AUDIO",
+                              "mediaObjects": [
+                                  {
+                                      "contentUrl": mp3Url,
+                                      "description": mp3Url,
+                                      "name": fileName
+                                  }
+                              ]
+                          }
+                      }
+                  ],
+                  "suggestions": [
+                      {
+                          "title": "Media"
+                      }
+                  ]
+              }
+          }
+      }
+    }
+};
+
+
+exports.log = (req, result) => {
+    console.info("request");
+    console.info(JSON.stringify(req.body));
+
+    console.info("response");
+    console.info(JSON.stringify(result));
+};
