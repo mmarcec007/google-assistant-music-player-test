fcf12b25dda520682ff778803057e11c0cec7798
Mark Marcec <mark@zef.dev>
2020-02-12 16:05:19 +0100

    add new responses in alexa

--- functions/responses/alexa/alexa.js
+++ functions/responses/alexa/alexa.js
@@ -1,13 +1,31 @@
-exports.getSimpleResponse = (parameters, detectedIntent, shouldEndSession) => {
+exports.getSimpleTestResponse = (parameters, detectedIntent, shouldEndSession) => {
     return {
-        version: "1.0",
-        sessionAttributes: {
+        "version": "1.0",
+        "sessionAttributes": {
             "key": parameters !== null ? parameters : "N/A"
         },
-        response: {
-            outputSpeech: {
-                type: "PlainText",
-                text: "Detected Intent is " + detectedIntent !== null ? detectedIntent : "not available!"
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": detectedIntent
+            },
+            "card": {
+                "type": "Simple",
+                "title": detectedIntent,
+                "content": "This is the card content. This card just has plain text content.\r\nThe content is formated with line breaks to improve readability."
+            },
+            "shouldEndSession": shouldEndSession
+        }
+    }
+};
+
+exports.getSimpleResponse = (text, shouldEndSession) => {
+    return {
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": text
             },
             "shouldEndSession": shouldEndSession
         }
