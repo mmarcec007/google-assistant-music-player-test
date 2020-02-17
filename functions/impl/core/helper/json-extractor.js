9e20ff8071cac2e3b3bd55ef0a7ca4981de4e3e0
Mark Marcec <mark@zef.dev>
2020-02-17 15:43:35 +0100

    add getSingleValueFromJson in json-extractor.js

--- functions/impl/core/helper/json-extractor.js
+++ functions/impl/core/helper/json-extractor.js
@@ -3,3 +3,7 @@ const jsonQuery = require('json-query');
 exports.getValuesFromJson = (jsonQueryString, jsonData) => {
     return jsonQuery(jsonQueryString, {data: jsonData}).value;
 };
+
+exports.getSingleValueFromJson = (jsonQueryString, jsonData) => {
+    return jsonQuery(jsonQueryString, {data: jsonData}).value[0];
+};
