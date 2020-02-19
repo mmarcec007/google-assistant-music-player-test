77fd5429c812c009b3196d32037ba9b0586cdd4f
Mark Marcec <mark@zef.dev>
2020-02-19 15:17:35 +0100

    add inContextAlive in dialogflow-helper.js

--- functions/impl/core/helper/dialogflow-helper.js
+++ functions/impl/core/helper/dialogflow-helper.js
@@ -2,6 +2,16 @@ exports.getContext = (outputContexts, contextName) => {
     return outputContexts.filter(item => {return item.name.includes(contextName)})[0];
 };
 
+exports.isContextAlive = (outputContexts, contextName) => {
+    const d = this.getContext(outputContexts, contextName);
+    console.log("Printing context data:");
+    console.log(d);
+    if (d && d.lifespanCount && d.lifespanCount > 0) {
+        return true;
+    }
+    return true;
+};
+
 exports.getCapability = (capabilities, capabilityName) => {
     return capabilities.filter(item => {return item.name.includes(capabilityName)})[0];
 };
